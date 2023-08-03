d4d32cf7f71dca699d473a8fa299491316bb448f
sizhouLiu <lsz8824523@163.com>
2023-08-03 11:34:03 +0800

    提高了封装性,迭代版本,删除了失效代码.

--- 爬虫.py
+++ 爬虫.py
@@ -49,70 +49,43 @@ def get_danmu():
     time.sleep(2)
 
 
-# def dorama_url_get():
-#     # url_1 = "https://v.qq.com/channel/tv/list?filter_params=ifeature%3D-1%26iarea%3D814%26iyear%3D2023%26ipay%3D-1%26sort%3D75&page_id=channel_list_second_page"
-#     url = "https://pbaccess.video.qq.com/trpc.vector_layout.page_view.PageService/getPage?video_appid=3000010"
-#     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.183'}
-#     cookies = {"Cookie":"pgv_pvid=184197000; RK=S20sAhgAH/; ptcz=77015465c8b1c3cf6c05b8fd49727f783a61aca766fa4ffaf42994df2f8236d8; pac_uid=1_1214602074; iip=0; eas_sid=a1X6I9n0F1U1w041v3s6K6r9C1; qq_domain_video_guid_verify=4cccd06f254eab04; video_platform=2; video_guid=4cccd06f254eab04; pgv_info=ssid=s5322851164; o_cookie=1214602074; vversion_name=8.2.95; video_omgid=4cccd06f254eab04; _video_qq_login_time_init=1690377511; qz_gdt=seq4czadaaaimuaejdja; login_time_last=2023-7-26 21:37:26"}
-#     Post_data = {
-#         'caller_id'
-#         :
-#             "3000010",
-#         "data_mode"
-#         :
-#             "default",
-#         "page_id":"100113",
-#         "page_type":"channel",
-#        "platform_id":"2", "user_mode": "defau"
-#     }
-#     dorama_url = requests.post(url=url,headers=headers,cookies=cookies,data=Post_data).text
-#
-#     print(dorama_url)
-#     json.loads(dorama_url)
-#
-#     dorama_text = dorama_url
-
-
-
-pinglun_data = []
-
-
-def Iqiyi(content_id,pinglun_data,last_id='&'):
+def Iqiyi(name, content_id,last_id='&'):
     '''
 
     :param last_id:评论的最后一个id在接口请求中能够看到
     :param pinglun_data: 存放数据的list
     :return: 无return值 保存为一个csv文件
     '''
-
-    # https: // sns - comment.iqiyi.com / v3 / comment / get_baseline_comments.action?agent_type = 118 & agent_version = 9.11
-    # .5 & authcookie = null & business_type = 17 & channel_id = 2 & content_id = 8010127344745600 & last_id = & need_vote = 1 & page_size = 10 & qyid = e5debc58fe819b25ca6de2fe991d92cc & sort = HOT & tail_num = 1 & callback = jsonp_1690944202555_34284
-    headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.183"}
-    sort=["TIMEDESC","HOT"]
-    url =f"https://sns-comment.iqiyi.com/v3/comment/get_baseline_comments.action?agent_type=118&agent_version=9.11.5&authcookie=null&business_type=17&channel_id=2&content_id={content_id}&last_id={last_id}&need_vote=1&page=NaN&page_size=40&qyid=e5debc58fe819b25ca6de2fe991d92cc&sort=HOT&tail_num=1&callback=jsonp_1690948653064_72360"
-    print(url)
-    Iqiyi_get = requests.get(url,headers=headers)
-    # 获取的数据被js外边包了一个壳 需要对数据进行处理后才能被json.loads解析
-    u1 = '{'+Iqiyi_get.text.lstrip('try{ jsonp_16904544092779_86397(').rstrip('}) }catch(e){};')+'}}' #删除获取json的外壳
-    # 这个壳我研究了很长时间 死活都没办法被json解析 后来我发现居然是因为我删除的时候多删除了一个括号MMP
-    json_data = json.loads(u1.encode("utf-8"))["data"]["comments"]
-    for comment in json_data:
-        print(comment["id"])
-        if 'content' in comment.keys():
-            pinglun_data.append([comment['content'],comment["userInfo"]["uname"]])
-        #更新last_id
-        last_id = comment["id"]
-    print(len(pinglun_data))
-
-
-
-    if len(pinglun_data) < 800:
+    pinglun_data = []
+    while len(pinglun_data) < 800:
         time.sleep(0.5)
-        Iqiyi(content_id,pinglun_data,last_id)
-        # 递归
+        headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.183"}
+        sort=["TIMEDESC","HOT"]
+        url =f"https://sns-comment.iqiyi.com/v3/comment/get_baseline_comments.action?agent_type=118&agent_version=9.11.5&authcookie=null&business_type=17&channel_id=2&content_id={content_id}&last_id={last_id}&need_vote=1&page=NaN&page_size=40&qyid=e5debc58fe819b25ca6de2fe991d92cc&sort=HOT&tail_num=1&callback=jsonp_1690948653064_72360"
+        print(url)
+        Iqiyi_get = requests.get(url, headers=headers)
+        # 获取的数据被js外边包了一个壳 需要对数据进行处理后才能被json.loads解析
+        u1 = '{'+Iqiyi_get.text.lstrip('try{ jsonp_16904544092779_86397(').rstrip('}) }catch(e){};')+'}}' #删除获取json的外壳
+        # 这个壳我研究了很长时间 死活都没办法被json解析 后来我发现居然是因为我删除的时候多删除了一个括号MMP
+        comment_cont = json.loads(u1.encode("utf-8"))["data"]["totalCount"]
+        json_data = json.loads(u1.encode("utf-8"))["data"]["comments"]
+        for comment in json_data:
+            print(comment["id"])
+            if 'content' in comment.keys():
+                pinglun_data.append([comment['content'],comment["userInfo"]["uname"]])
+            #更新last_id
+            last_id = comment["id"]
+        print(len(pinglun_data))
+        if comment_cont-10 <= len(pinglun_data):
+            pd.DataFrame(pinglun_data).to_csv(f"./评论CSV/{name}.csv", mode="w")
+            pinglun_data.clear()
+            break
+
+
     else:
-        pd.DataFrame(pinglun_data).to_csv("理科生坠入情网.csv",mode="a")
-        return pinglun_data
+        pd.DataFrame(pinglun_data).to_csv(f"./评论CSV/{name}.csv", mode="w")
+        pinglun_data.clear()
+        return
     # 保存为csv
 
 
@@ -130,12 +103,13 @@ def dorama_data_get():
             for comment in json_data:
                 data_list = [comment["title"], comment["description"], comment["tag"], comment["showDate"],
                              [i["name"] for i in comment['creator']], [i["name"] for i in comment["contributor"]]
-                            ,comment["page_url"],comment["image_url_normal"],comment["time_length"],comment["hot_score"]]
+                            , comment["page_url"], comment["image_url_normal"], comment["time_length"], comment["hot_score"], comment["tv_id"]]
                 print(comment["page_url"])
                 print(comment["image_url_normal"])
                 print(data_list)
                 alldata_list.append(data_list)
                 time.sleep(0.5)
+
                 ## 这里是尝试使用接口获取数据,但是爱奇艺的接口被加密过,我没有办法自动获取接口进行爬取搁置在此
 
                 # dorama_data = requests.get(comment["page_url"], headers=headers).text
@@ -157,34 +131,26 @@ def dorama_data_get():
     except:
         mkdir("./爱奇艺数据/电视剧海报")
         for name in alldata_list:
-            picture_resp = requests.get(name[-3])
+            picture_resp = requests.get(name[-4])
             with open(f"./爱奇艺数据/电视剧海报/{name[0]}.jpg", mode='wb') as f:
                 f.write(picture_resp.content)
-        df = pd.DataFrame(alldata_list, columns=["title","description","tag","showDate", 'creators', "contributor", "page_url","img_url","time_length","hot_score"])
+        df = pd.DataFrame(alldata_list, columns=["title","description","tag","showDate", 'creators', "contributor", "page_url", "img_url", "time_length", "hot_score", "content_id"])
         df.to_csv("./爱奇艺数据/爱奇艺热播.csv")
         return alldata_list
 
 
-# def TXvideourl_get():
-#     url = "https://youku.com/channel/webtv/list?filter=type_%E7%94%B5%E8%A7%86%E5%89%A7_year_2023&spm=a2hja.14919748_WEBTV_JINGXUAN.drawer3.d_main_area_1"
-#     # headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.183', "Cookie":"pgv_pvid=184197000; RK=S20sAhgAH/; ptcz=77015465c8b1c3cf6c05b8fd49727f783a61aca766fa4ffaf42994df2f8236d8; pac_uid=1_1214602074; iip=0; eas_sid=a1X6I9n0F1U1w041v3s6K6r9C1; qq_domain_video_guid_verify=4cccd06f254eab04; video_platform=2; video_guid=4cccd06f254eab04; pgv_info=ssid=s5322851164; o_cookie=1214602074; vversion_name=8.2.95; video_omgid=4cccd06f254eab04; _video_qq_login_time_init=1690377511; qz_gdt=seq4czadaaaimuaejdja; login_time_last=2023-7-26 21:37:26"}
-#     data = requests.get(url=url, headers=headers).text
-#     obj = re.compile(r'<div class="videolist_container_inner">.*?<a class="aplus_exp aplus_clk" herf="(?P<url>.*?)"title="(?P<name>.*?)"target=.*?<img s'
-#                      r'rc="(?P<img_url>.*?)"',re.S)
-#     res = obj.finditer(data)
-#     for i in res:
-#         print(i)
+def comment_data_get(path):
+    df = pd.read_csv(path)
+    for title, content_id in df.iterrows():
+        Iqiyi(content_id[1], content_id[-1])
 
 
 def youku():
-    print(1)
     TV_Data = []
     driver = webdriver.Chrome(executable_path="./chromedriver.exe")
-    print(1)
     driver.get("https://www.youku.com/channel/webtv/list?filter=type_%E7%94%B5%E8%A7%86%E5%89%A7_year_2023&spm=a2hja.14919748_WEBTV_JINGXUAN.drawer3.d_sort_1")
     css_selectors = ["#app > div.app_normal.window > div.modulelist_s_body > div.videolist_s_body > div > div > div:nth-child(4) > div > div:nth-child({}) > div > div > div.categorypack_info_list > div.categorypack_title.categorypack_short_title > a".format(str(i)) for i in range(41)]
     for css_selector in css_selectors:
-        print("1")
         if css_selector == "#app > div.app_normal.window > div.modulelist_s_body > div.videolist_s_body > div > div > div:nth-child(4) > div > div:nth-child(0) > div > div > div.categorypack_info_list > div.categorypack_title.categorypack_short_title > a":
             pass
         else:
@@ -213,8 +179,7 @@ if __name__ == "__main__":
     # dorama_url_get()
     # TXvideourl_get()
     # dorama_url_get()
-    Iqiyi("4892910860430400", pinglun_data)
+    # Iqiyi("测试","1116314049607400")
     # dorama_data_get()
-
-
+    comment_data_get("./爱奇艺数据/爱奇艺热播.csv")
 
